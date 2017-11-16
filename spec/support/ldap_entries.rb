d788cafca7253be414cdd1f9cb95d0388f581053
Carla Galarza <cmg2228@columbia.edu>
2017-11-16 16:36:18 -0500

    Adding test for entry with undefined mail field.

--- spec/support/ldap_entries.rb
+++ spec/support/ldap_entries.rb
@@ -25,6 +25,29 @@ objectClass: eduPerson}
       )
   end
 
+  let(:ldap_entry_without_mail) do
+    Net::LDAP::Entry.from_single_ldif_string(
+%Q{dn: uni=#{uni},ou=People,o=Columbia University,c=US
+departmentNumber: 2206202
+campusphone: MS 1-0000
+sn: Doe
+ou: Columbia University Libraries
+uid: #{uni}
+givenName: Jane
+uni: #{uni}
+postalAddress: 201 IAB$Mail Code: 3301$United States
+cn: Jane Doe
+telephoneNumber: +1 212 851 2903
+title: Librarian
+objectClass: person
+objectClass: organizationalPerson
+objectClass: inetOrgPerson
+objectClass: cuPerson
+objectClass: cuRestricted
+objectClass: eduPerson}
+      )
+  end
+
   let(:ldap_entries) {
     [Cul::LDAP::Entry.new(ldap_entry)]
   }
